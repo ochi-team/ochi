cb1029d62a4ce3909d4c537d5addf1e4a4a4f3bf
Ratushniak DA <ratushniak_da@groupbwt.com>
2026-03-08 12:57:02 +0200

    rename fn tenant isValidID

--- src/store/tenant.zig
+++ src/store/tenant.zig
@@ -2,6 +2,6 @@ pub const TenantID = []const u8;
 
 pub const maxTenantIDLen = 16;
 
-pub fn checkID(value: TenantID) bool {
+pub fn isValidID(value: TenantID) bool {
     return value.len <= maxTenantIDLen;
 }
