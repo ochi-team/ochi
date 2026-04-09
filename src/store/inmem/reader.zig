ae6388ea4fef37b2d35530ead6586758f25b7053
Marko Kostic <marko03kostic@protonmail.com>
2026-04-09 18:14:01 +0200

    remove constCast, make fields non const pointers

--- src/store/inmem/reader.zig
+++ src/store/inmem/reader.zig
@@ -33,8 +33,8 @@ pub const StreamReader = struct {
     bloomTokensList: std.ArrayList([]const u8),
 
     // TODO: decode manually when it comes to file reader
-    columnIDGen: *const ColumnIDGen,
-    colIdx: *const std.AutoHashMap(u16, u16),
+    columnIDGen: *ColumnIDGen,
+    colIdx: *std.AutoHashMap(u16, u16),
 
     // TODO: this flag doesn't look smart,
     // there must be a better idea to track ownership
@@ -218,12 +218,10 @@ pub const StreamReader = struct {
         allocator.free(self.columnsKeysBuf);
         allocator.free(self.columnIdxsBuf);
 
-        const columnIDGen = @constCast(self.columnIDGen);
-        columnIDGen.deinit(allocator);
+        self.columnIDGen.deinit(allocator);
 
-        const colIdx = @constCast(self.colIdx);
-        colIdx.deinit();
-        allocator.destroy(colIdx);
+        self.colIdx.deinit();
+        allocator.destroy(self.colIdx);
 
         allocator.destroy(self);
     }
