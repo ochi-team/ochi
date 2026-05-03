835012bd7d554615a6e131209c91fa074285615b
Marko Kostic <marko03kostic@protonmail.com>
2026-05-03 20:48:52 +0200

    use recommended read method, add todo

--- src/store/index/MetaIndex.zig
+++ src/store/index/MetaIndex.zig
@@ -75,8 +75,10 @@ pub fn readFile(io: Io, alloc: Allocator, path: []const u8, blocksCount: u64) !D
 
     var metaindexFileReader = metaindexFile.reader(io, &.{});
 
-    const metaindexCompressed = try metaindexFileReader.interface.readAlloc(fbaAlloc, metaindexStat.size);
+    const metaindexCompressed = try metaindexFileReader.interface.allocRemaining(fbaAlloc, .unlimited);
     defer fbaAlloc.free(metaindexCompressed);
+    // TODO why does .limited(metaindexStat.size) not work above?
+    std.debug.assert(metaindexStat.size + 1 > metaindexCompressed.len);
 
     const decodedMetaindex = try MetaIndex.decodeDecompress(alloc, metaindexCompressed, blocksCount);
     std.debug.assert(decodedMetaindex.compressedSize == metaindexStat.size);
