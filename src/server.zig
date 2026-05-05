f361bcfcbe8eaaf76a42b8ad49130a8f10b1a7ad
Denis Dvornikov <candyboobers@gmail.com>
2026-05-05 18:12:32 +0200

    kill me

--- src/server.zig
+++ src/server.zig
@@ -104,3 +104,10 @@ test "serverWithSIGTERM" {
     // Send SIGTERM to ourselves
     try std.posix.kill(std.c.getpid(), std.posix.SIG.TERM);
 }
+
+test {
+    std.testing.refAllDecls(@This());
+    _ = @import("server_test.zig");
+    _ = @import("tidy.zig");
+    std.testing.refAllDecls(@import("query/Loql.zig"));
+}
