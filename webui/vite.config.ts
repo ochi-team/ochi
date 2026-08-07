7bb50c02f90b2ad871d2485d39ced128e285532b
Denis Dvornikov <candyboobers@gmail.com>
2026-08-07 19:00:39 +0200

    support ui remote host

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,17 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
-export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/query': 'http://127.0.0.1:9014',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', 'OCHI');
+  const host = env.OCHI_HOST || 'http://127.0.0.1:9014';
+
+  return {
+    envPrefix: 'OCHI_',
+    plugins: [devtools(), solidPlugin(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/query': host,
+      },
     },
-  },
-  build: {
-    target: 'esnext',
-  },
+    build: {
+      target: 'esnext',
+    },
+  };
 });
